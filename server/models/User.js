c7fb7095702c1020928d2c8f75a6f732d6d95b66
Ruben <ruben.alston646@gmail.com>
2025-05-10 22:53:00 -0400

    refactor: simplify user schema and remove password hashing logic

--- server/models/User.js
+++ server/models/User.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+  name: String,
+  email: String,
+  password: String,
 });
 
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
+userSchema.set("toJSON", {
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  },
 });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);

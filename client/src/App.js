3a19af971d620982f7af97a9b952b0f7b22d5408
Ruben <ruben.alston646@gmail.com>
2025-05-25 20:02:41 -0400

    feat: refactor MainLayout component and update routing in App for improved structure

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import JobListings from "./pages/JobListings";
 import JobApplication from "./pages/JobApplication";
 import Register from "./pages/Register";
 import LandingPage from "./pages/LandingPage"; // <-- Add this import
-import MainLayout from "./pages/MainLayout";
+import MainLayout from "./components/MainLayout";
 import "./App.css";
 
 function App() {
@@ -19,9 +19,11 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={
-            <MainLayout>
-              <Dashboard />
-            </MainLayout>
+            <ProtectedRoute>
+              <MainLayout>
+                <Dashboard />
+              </MainLayout>
+            </ProtectedRoute>
           } />
           <Route path="/jobs" element={
             <ProtectedRoute>

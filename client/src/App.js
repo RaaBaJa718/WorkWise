51bb23ad7d80d303c9b25308cb0c1040de7344e5
Ruben <ruben.alston646@gmail.com>
2025-05-26 09:57:05 -0400

    fix: add missing React import to App.js for proper component functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";

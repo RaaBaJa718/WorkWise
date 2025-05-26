3fcfb9366dbd6cf25a849dd3000eb9e9ca555b12
Ruben <ruben.alston646@gmail.com>
2025-05-26 09:56:14 -0400

    refactor: restructure routing logic into separate AppRoutes component for improved organization

--- client/src/App.js
+++ client/src/App.js
@@ -6,42 +6,47 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import JobListings from "./pages/JobListings";
 import JobApplication from "./pages/JobApplication";
 import Register from "./pages/Register";
-import LandingPage from "./pages/LandingPage"; // <-- Add this import
+import LandingPage from "./pages/LandingPage";
 import MainLayout from "./components/MainLayout";
 import "./App.css";
-import { useContext } from "react";
-import AuthContext from "./context/AuthContext";
 
-function App() {
-  const { user } = useContext(AuthContext);
+function AppRoutes() {
+  // Now it's safe to use useContext here, because this is a child of AuthProvider
+  const { user } = React.useContext(require("./context/AuthContext").default);
 
   return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/dashboard" /> : <Login />}
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <MainLayout>
+              <Dashboard />
+            </MainLayout>
+          </ProtectedRoute>
+        } />
+        <Route path="/jobs" element={
+          <ProtectedRoute>
+            <MainLayout>
+              <JobListings />
+            </MainLayout>
+          </ProtectedRoute>
+        } />
+        <Route path="/apply" element={<JobApplication />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function App() {
+  return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} /> {/* Add this route */}
-          <Route
-            path="/login"
-            element={user ? <Navigate to="/dashboard" /> : <Login />}
-          />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <MainLayout>
-                <Dashboard />
-              </MainLayout>
-            </ProtectedRoute>
-          } />
-          <Route path="/jobs" element={
-            <ProtectedRoute>
-              <MainLayout>
-                <JobListings />
-              </MainLayout>
-            </ProtectedRoute>
-          } />
-          <Route path="/apply" element={<JobApplication />} />
-        </Routes>
-      </Router>
+      <AppRoutes />
     </AuthProvider>
   );
 }
